96661203a85a4b1c83b0dd17a1e29c06496e5753
LordOfCinder2 <matias.dilisa@gmail.com>
2023-03-31 19:29:38 -0300

    continuacion estilos

--- src/Components/Layout/NavbarLayout/NavbarLayout.jsx
+++ src/Components/Layout/NavbarLayout/NavbarLayout.jsx
@@ -1,9 +1,9 @@
 import React, { useContext, useEffect } from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
 import { CartContextReducer } from '../../../context/CartContextReducer'
-import { Button } from '@mui/material'
-import styles from './Navbar.module.css'
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Button, ButtonGroup } from '@mui/material'
+import './NavbarLayout.css'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
 
 const NavbarLayout = () => {
 	const { state, dispatch } = useContext(CartContextReducer)
@@ -12,34 +12,38 @@ const NavbarLayout = () => {
 		dispatch({ type: 'GET_TOTAL_QUANTITY' })
 	}, [state.cart])
 
-	return (<div>
-		<div className={styles.header}>
-		<div className={styles.navBar}>
-			<NavLink
-				to="/login"
-				className={({ isActive }) => (isActive ? 'activePage' : 'Page')}
-			>
-				<Button variant='outlined'>
-					Ir al login
-					</Button>
-			</NavLink>
-			<NavLink
-				to="/shop"
-				className={({ isActive }) => (isActive ? 'activePage' : 'Page')}
-			>
-				<Button variant='outlined'>Tienda</Button>
-			</NavLink>
-			<NavLink
-				to="/cart"
-				className={({ isActive }) => (isActive ? 'activePage' : 'Page')}
-			>
-				<Button variant='outlined'>Ir al carrito</Button>
-			</NavLink>
-		</div>
-			<h3><ShoppingCartIcon/>{state.totalQuantity} </h3>
-		</div>
+	return (
+		<div>
+			<div className="header">
+				<div className="nav-bar">
+					<ButtonGroup variant='text' aria-label='text button group'>
+					<NavLink
+						to="/login"
+						className={({ isActive }) => (isActive ? 'active-page' : 'page')}
+					>
+						<Button className='botones'>Ir al login</Button>
+					</NavLink>
+					<NavLink
+						to="/shop"
+						className={({ isActive }) => (isActive ? 'active-page' : 'page')}
+					>
+						<Button className='botones'>Tienda</Button>
+					</NavLink>
+					<NavLink
+						to="/cart"
+						className={({ isActive }) => (isActive ? 'active-page' : 'page')}
+					>
+						<Button className='botones'>Ir al carrito</Button>
+					</NavLink>
+					</ButtonGroup>
+				</div>
+				<h3>
+					<span>{state.totalQuantity}</span>
+					<ShoppingCartIcon fontSize='small'/>
+				</h3>
+			</div>
 			<Outlet />
-	</div>
+		</div>
 	)
 }
 
